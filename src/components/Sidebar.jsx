a74fc713f90876d595b56fe03d9cf73e039273fe
Gabriel <gabrielsideprojects@gmail.com>
2023-02-14 20:52:22 -0300

    Sidebar comp

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,25 @@
 import styles from './Sidebar.module.css'
-
+import {PencilLine } from 'phosphor-react'
 export function Sidebar(){
     return (
         <aside className={styles.sidebar}>
-            <img src='https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50' />
+            <img 
+            className={styles.cover}
+             src='https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50' />
             <div className={styles.profile}>
-                <strong>Diego Fernandes</strong>
+                <img className={styles.avatar} src="https://github.com/gabrielsideprojects.png" />
+
+
+                <strong>Gabriel Menezes</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href='#'> Editar seu perfil</a>
+   
+                <a href='#'> 
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
